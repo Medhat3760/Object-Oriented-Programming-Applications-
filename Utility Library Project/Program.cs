a5beb6092a673d5501b0cc720dc350a3c6a354b8
Medhat3760 <Medhat3760@gmail.com>
2025-07-30 14:03:17 +0200

    Project 1: Utility Library

---
+++ Utility Library Project/Program.cs
@@ -0,0 +1,144 @@
+﻿using Date_Library_Project;
+namespace Utility_Library_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(clsUtil.RandomNumber(1, 10));
+            Console.WriteLine(clsUtil.GetRandomCharacter(clsUtil.enCharType.MixChar));
+            Console.WriteLine(clsUtil.GenerateWord(clsUtil.enCharType.MixChar, 8));
+            Console.WriteLine(clsUtil.GenerateKey(clsUtil.enCharType.MixChar));
+            clsUtil.GenerateKeys(10, clsUtil.enCharType.MixChar);
+
+            Console.WriteLine();
+
+            int x = 10, y = 20;
+            Console.WriteLine(x + " " + y);
+            clsUtil.Swap(ref x, ref y);
+            Console.WriteLine(x + " " + y);
+
+            Console.WriteLine();
+
+            double a = 10.5, b = 20.5;
+            Console.WriteLine(a + " " + b);
+            clsUtil.Swap(ref a, ref b);
+            Console.WriteLine(a + " " + b);
+
+            Console.WriteLine();
+
+            string s1 = "Ali", s2 = "Ahmed";
+            Console.WriteLine(s1 + " " + s2);
+            clsUtil.Swap(ref s1, ref s2);
+            Console.WriteLine(s1 + " " + s2);
+
+            Console.WriteLine();
+
+            char ch1 = 'A', ch2 = 'B';
+            Console.WriteLine(ch1 + " " + ch2);
+            clsUtil.Swap(ref ch1, ref ch2);
+            Console.WriteLine(ch1 + " " + ch2);
+
+            Console.WriteLine();
+
+            bool flag1 = true, flag2 = false;
+            Console.WriteLine(flag1 + " " + flag2);
+            clsUtil.Swap(ref flag1, ref flag2);
+            Console.WriteLine(flag1 + " " + flag2);
+
+            Console.WriteLine();
+
+            clsDate d1 = new clsDate(1, 10, 2022), d2 = new clsDate(1, 1, 2022);
+            Console.WriteLine(d1.DateToString() + " " + d2.DateToString());
+            clsUtil.Swap(ref d1, ref d2);
+            Console.WriteLine(d1.DateToString() + " " + d2.DateToString());
+
+            // Shuffle Array
+
+            int[] arr1 = { 1, 2, 3, 4, 5 };
+
+            clsUtil.ShuffleArray(arr1, arr1.Length);
+
+            Console.WriteLine("\nArray 1 After Shuffle:");
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+
+                Console.Write(arr1[i] + " ");
+
+            }
+            Console.WriteLine();
+
+            string[] arr2 = { "Abdelrahman", "Medhat", "Rushdy", "Tawfik" };
+
+            clsUtil.ShuffleArray(arr2, arr2.Length);
+
+            Console.WriteLine("\nArray 2 After Shuffle:");
+
+            for (int i = 0; i < arr2.Length; i++)
+            {
+
+                Console.Write(arr2[i] + " ");
+
+            }
+            Console.WriteLine();
+
+            int[] arr3 = new int[10];
+            int arrLength = 5;
+            clsUtil.FillArrayWithRandomNumbers(arr3, arrLength, 1, 100);
+
+            clsUtil.AddArrayElement(-100, arr3, ref arrLength);
+
+            Console.WriteLine("\nArray 3 After Fill:");
+
+            for (int i = 0; i < arrLength; i++)
+            {
+
+                Console.Write(arr3[i] + " ");
+
+            }
+            Console.WriteLine();
+
+            string[] arr4 = new string[5];
+
+            clsUtil.FillArrayWithRandomWords(arr4, 5, clsUtil.enCharType.MixChar, 8);
+
+            Console.WriteLine("\nArray 4 After Fill:");
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                Console.WriteLine(arr4[i]);
+
+            }
+
+            string[] arr5 = new string[5];
+
+            clsUtil.FillArrayWithRandomKeys(arr5, 5, clsUtil.enCharType.MixChar);
+
+            Console.WriteLine("\nArray 5 After Fill:");
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                Console.WriteLine(arr5[i]);
+
+            }
+
+            Console.WriteLine($"\nText1{clsUtil.Tabs(5)}Text2");
+
+            const short encryptionKey = 2;
+
+            string textAfterEncryption, textAfterDecryption;
+            string text = "Abdelrahman Medhat";
+
+            textAfterEncryption = clsUtil.EncryptText(text, encryptionKey);
+            textAfterDecryption = clsUtil.DecryptText(textAfterEncryption, encryptionKey);
+
+            Console.WriteLine("\nText After Encryption: " + textAfterEncryption);
+            Console.WriteLine("Text After Decryption: " + textAfterDecryption);
+
+        }
+    }
+}
---
+++ Utility Library Project/clsUtil.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Date_Library_Project;
+namespace Utility_Library_Project
+{
+    public class clsUtil
+    {
+
+        public static string AlignNumberColumn(int number)
+        {
+            if (number < 10)
+                return "     : ";
+            else if (number < 100)
+                return "    : ";
+            else if (number < 1000)
+                return "   : ";
+            else if (number < 10000)
+                return "  : ";
+            else if (number < 100000)
+                return " : ";
+            else
+                return ": ";
+        }
+  
+        public static string Tabs(short numberOfTabs)
+        {
+
+            StringBuilder sb = new StringBuilder();
+
+            for(short i =1; i <= numberOfTabs; i++)
+            {
+                sb.Append("\t");
+            }
+
+            return sb.ToString();
+
+        }
+
+        private static Random _random = new Random();
+
+        public static int RandomNumber(int from, int to)
+        {
+
+            return _random.Next(from, to + 1);
+
+        }
+
+        public enum enCharType { SmallLetter = 1, CapitalLetter = 2, Digit = 3, MixChar = 4, SpecialCharacter = 5 }
+
+        public static char GetRandomCharacter(enCharType charType)
+        {
+
+            if (charType == enCharType.MixChar)
+            {
+                charType = (enCharType)RandomNumber(1, 3);
+            }
+
+            switch (charType)
+            {
+
+                case enCharType.SmallLetter:
+                    return (char)RandomNumber(97, 122); // ASCII range for lowercase letters
+
+                case enCharType.CapitalLetter:
+                    return (char)RandomNumber(65, 90); // ASCII range for uppercase letters
+
+                case enCharType.SpecialCharacter:
+                    return (char)RandomNumber(33, 47); // ASCII range for special characters
+
+                case enCharType.Digit:
+                    return (char)RandomNumber(48, 57); // ASCII range for digits
+
+                default:
+                    return ' ';
+
+            }
+
+        }
+ 
+        public static string GenerateWord(enCharType charType, short length)
+        {
+
+            StringBuilder word = new StringBuilder(); 
+
+            for (int i = 1; i <= length; i++)
+            {
+
+                word.Append(GetRandomCharacter(charType));
+
+            }
+
+            return word.ToString();
+
+        }
+
+        public static string GenerateKey(enCharType charType)
+        {
+
+            string key;
+
+            key = GenerateWord(charType, 4) + "-"
+                + GenerateWord(charType, 4) + "-"
+                + GenerateWord(charType, 4) + "-"
+                + GenerateWord(charType, 4);
+
+            return key;
+
+        }
+
+        public static void GenerateKeys(short numberOfKeys, enCharType charType)
+        {
+
+            for (int i = 1; i <= numberOfKeys; i++)
+            {
+
+                Console.WriteLine($"Key [{i}]"+ AlignNumberColumn(i) + GenerateKey(charType));
+            }
+        }
+
+        public static void Swap(ref int a, ref int b)
+        {
+
+            int temp;  // Temporary variable to hold one of the values
+            temp = a;  // Store the value of A in Temp
+            a = b;     // Assign the value of B to A
+            b = temp;  // Assign the value stored in Temp (which is the original A) to B
+
+        }
+
+        public static void Swap(ref double a, ref double b)
+        {
+
+            double temp = a;
+            a = b;
+            b = temp;
+
+        }
+        
+        public static void Swap(ref string a, ref string b)
+        {
+
+            string temp = a;
+            a = b;
+            b = temp;
+
+        }
+        
+        public static void Swap(ref char a, ref char b)
+        {
+
+            char temp = a;
+            a = b;
+            b = temp;
+
+        }
+
+        public static void Swap(ref bool a, ref bool b)
+        {
+
+            bool temp = a;
+            a = b;
+            b = temp;
+
+        }
+
+        public static void Swap(ref clsDate a, ref clsDate b)
+        {
+
+            clsDate.SwapDates(ref a, ref b);
+
+        }
+
+        public static void ShuffleArray(int[] arr, int arrLength)
+        {
+
+            for (int i = 0; i < arrLength; i++)
+            {
+
+                Swap(ref arr[i], ref arr[RandomNumber(0, arrLength - 1)]); 
+
+            }
+
+        }
+
+        public static void ShuffleArray(string[] arr, int arrLength)
+        {
+
+            for (int i = 0; i < arrLength; i++)
+            {
+
+                Swap(ref arr[i], ref arr[RandomNumber(0, arrLength - 1)]);
+
+            }
+
+        }
+
+
+        public static void FillArrayWithRandomNumbers(int[] arr, int length, int from, int to)
+        {
+
+            for (int i = 0; i < length; i++)
+            {
+
+                arr[i] = RandomNumber(from, to);
+
+            }
+
+        }
+        
+        public static void FillArrayWithRandomWords(string[] arr, int length, enCharType charType, short wordLength)
+        {
+
+            for (int i = 0; i < length; i++)
+            {
+
+                arr[i] = GenerateWord(charType, wordLength);
+
+            }
+
+        }
+        
+        public static void FillArrayWithRandomKeys(string[] arr, int length, enCharType charType)
+        {
+
+            for (int i = 0; i < length; i++)
+            {
+
+                arr[i] = GenerateKey(charType);
+
+            }
+
+        }
+
+        public static void AddArrayElement(int number, int[] arr, ref int arrLength)
+        {
+
+            arrLength++; //its a new element so we need to increase the array length by 1
+            arr[arrLength - 1] = number;
+
+        }
+
+        public static string EncryptText(string text, short encryptionKey)
+        {
+
+            char[] arrChar = text.ToCharArray();
+
+            for(int i=0; i < arrChar.Length; i++)
+            {
+
+                arrChar[i] =  (char)(arrChar[i] + encryptionKey);
+
+            }
+
+            return new string(arrChar);
+            
+        }
+        
+        public static string DecryptText(string text, short encryptionKey)
+        {
+
+            char[] arrChar = text.ToCharArray();
+
+            for(int i=0; i < arrChar.Length; i++)
+            {
+
+                arrChar[i] =  (char)(arrChar[i] - encryptionKey);
+
+            }
+
+            return new string(arrChar);
+            
+        }
+
+
+
+    }
+}
