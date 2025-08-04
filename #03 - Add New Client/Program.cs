34d72fbc39d111029c666257f6ed0a7f511fb34a
Medhat3760 <Medhat3760@gmail.com>
2025-08-04 21:18:14 +0200

    Add New Client

---
+++ #03 - Add New Client/Program.cs
@@ -0,0 +1,81 @@
+﻿using MyLib;
+namespace Bank_System
+{
+    internal class Program
+    {
+
+        static void ReadClientInfo(ref clsBankClient client)
+        {
+
+            Console.Write("\nEnter FirstName: ");
+            client.FirstName = clsInputValidate.ReadString();
+
+            Console.Write("\nEnter LastName: ");
+            client.LastName = clsInputValidate.ReadString();
+
+            Console.Write("\nEnter Email: ");
+            client.Email = clsInputValidate.ReadString();
+
+            Console.Write("\nEnter Phone: ");
+            client.Phone = clsInputValidate.ReadString();
+
+            Console.Write("\nEnter PinCode: ");
+            client.PinCode = clsInputValidate.ReadString();
+
+            Console.Write("\nEnter Account Balance: ");
+            client.AccountBalance = clsInputValidate.ReadFloatNumber();
+
+        }
+
+        static void AddNewClient()
+        {
+
+            Console.Write("\nPlease Enter Account Number: ");
+            string accountNumber = clsInputValidate.ReadString();
+
+            while (clsBankClient.IsClientExist(accountNumber))
+            {
+
+                Console.Write("\nAccount Number Is Already Used, Choose another one: ");
+                accountNumber = clsInputValidate.ReadString();
+
+            }
+
+            clsBankClient newClient = clsBankClient.GetAddNewClientObject(accountNumber);
+
+            ReadClientInfo(ref newClient);
+
+            clsBankClient.enSaveResult saveResult;
+
+            saveResult = newClient.Save();
+
+            switch (saveResult)
+            {
+
+                case clsBankClient.enSaveResult.svSucceded:
+                    Console.WriteLine("\nAccount Added Successfuly :-) ");
+                    newClient.Print();
+                    break;
+
+                case clsBankClient.enSaveResult.svFailedEmptyObject:
+                    Console.WriteLine("\nError account was not saved because it's Empty");
+                    break;
+
+                case clsBankClient.enSaveResult.svFailedAccountNumberExists:
+                    Console.WriteLine("\nError account was not saved because account number is used!");
+                    break;
+
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            AddNewClient();
+
+            Console.ReadKey();
+
+        }
+    }
+}
