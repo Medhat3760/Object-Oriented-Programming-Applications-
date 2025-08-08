a773198b350db85d2b3e1356c293b529966b5cf1
Medhat3760 <Medhat3760@gmail.com>
2025-08-08 22:48:48 +0200

    Added a Delete Client

---
+++ #04 - Delete Client/Program.cs
@@ -0,0 +1,55 @@
+﻿using MyLib;
+namespace Bank_System
+{
+    internal class Program
+    {
+
+        static void DeleteClient()
+        {
+
+            Console.Write("\nPlease enter Account Number: ");
+            string accountNumber = clsInputValidate.ReadString();
+
+            while (!clsBankClient.IsClientExist(accountNumber))
+            {
+
+                Console.Write("\nAccount Number is not found, choose another one: ");
+                accountNumber = clsInputValidate.ReadString();
+
+            }
+
+            clsBankClient client1 = clsBankClient.Find(accountNumber);
+            client1.Print();
+
+            Console.Write("\nAre you sure you want to delete this client? (Y/N)? ");
+            char confirm = char.Parse(Console.ReadLine());
+
+            if (confirm == 'Y' || confirm == 'y')
+            {
+
+                if (client1.Delete())
+                {
+
+                    Console.WriteLine("\nClient Deleted Successfully :-)");
+                    client1.Print();
+
+                }
+                else
+                {
+                    Console.WriteLine("\nError Client Was Not Deleted");
+                }
+
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            DeleteClient();
+
+            Console.ReadKey();
+
+        }
+    }
+}
