ef22cb7e97b1f1cb122bb8b8a5780c2900d63a86
Medhat3760 <Medhat3760@gmail.com>
2025-09-02 22:42:19 +0200

    Added a Currency Calculator Screen

---
+++ #41 - Currency Calculator Screen/clsTransferLogScreen.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_System
+{
+    public class clsTransferLogScreen : clsScreen
+    {
+
+        private static void _PrintTransferLogRecordLine(clsBankClient.stTransferLogRecord transferLogRecord)
+        {
+
+            Console.Write($"{"".PadLeft(10)}| {transferLogRecord.dateTime.PadRight(22)}");
+            Console.Write($"| {transferLogRecord.sourceAccountNumber.PadRight(10)}");
+            Console.Write($"| {transferLogRecord.destinationAccountNumber.PadRight(10)}");
+            Console.Write($"| {transferLogRecord.amount.ToString().PadRight(12)}");
+            Console.Write($"| {transferLogRecord.srcBalanceAfter.ToString().PadRight(12)}");
+            Console.Write($"| {transferLogRecord.destBalanceAfter.ToString().PadRight(12)}");
+            Console.Write($"| {transferLogRecord.userName.PadRight(10)}");
+
+        }
+
+        public static void ShowTransferLogScreen()
+        {
+
+            List<clsBankClient.stTransferLogRecord> listTransferLogRecord = clsBankClient.GetTransfersLogList();
+
+            string title = "\t Transfer Log List Screen";
+            string subTitle = $"\t      ({listTransferLogRecord.Count}) Record(s).";
+
+            _DrawScreenHeader(title, subTitle);
+
+            Console.WriteLine($"\n{"".PadLeft(10)}{new string('_',100)}\n");
+
+            Console.Write($"{"".PadLeft(10)}| {"DateTime".PadRight(22)}");
+            Console.Write($"| {"s.Acc".PadRight(10)}");
+            Console.Write($"| {"d.Acc".PadRight(10)}");
+            Console.Write($"| {"Amount".PadRight(12)}");
+            Console.Write($"| {"s.Balance".PadRight(12)}");
+            Console.Write($"| {"d.Balance".PadRight(12)}");
+            Console.Write($"| {"User".PadRight(10)}");
+
+            Console.WriteLine($"\n{"".PadLeft(10)}{new string('_', 100)}\n");
+
+            if(listTransferLogRecord.Count == 0)
+            {
+
+                Console.WriteLine("\n\t\t\t\t\tNo Transfers Available in The System!");
+
+            }
+            else
+            {
+
+                foreach(clsBankClient.stTransferLogRecord record in listTransferLogRecord)
+                {
+
+                    _PrintTransferLogRecordLine(record);
+                    Console.WriteLine();
+
+                }
+
+            }
+
+            Console.WriteLine($"\n{"".PadLeft(10)}{new string('_', 100)}\n");
+
+        }
+
+    }
+}
